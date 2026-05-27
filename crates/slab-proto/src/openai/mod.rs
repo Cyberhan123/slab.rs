1e2ba411e0a56d6eb8ae56f6ed63d7a93f5cf529
Cyberhan123 <255542417@qq.com>
2026-05-27 19:13:02 +0800

    fix: rust doc

--- crates/slab-proto/src/openai/mod.rs
+++ crates/slab-proto/src/openai/mod.rs
@@ -1,10 +1,10 @@
 #![allow(clippy::too_many_arguments)]
 pub(crate) mod models;
 
-///! Re-export generated DTOs directly at crate::openai::* to avoid deep paths.
-///! these dtos write from crates\slab-proto\openai\openapi\openapi.yaml by hand.
-///! these are not meant is all right if need, please see openapi.yaml and change the dto to the right way, and add the test case in tests folder
-///! No client code or server code should be added here, only the dto and test case for it, if you want to add client or server code, please add it in the client or server module respectively
+/// Re-export generated DTOs directly at crate::openai::* to avoid deep paths.
+/// these dtos write from crates\slab-proto\openai\openapi\openapi.yaml by hand.
+/// these are not meant is all right if need, please see openapi.yaml and change the dto to the right way, and add the test case in tests folder
+/// No client code or server code should be added here, only the dto and test case for it, if you want to add client or server code, please add it in the client or server module respectively
 pub use models::*;
 
 #[cfg(test)]
